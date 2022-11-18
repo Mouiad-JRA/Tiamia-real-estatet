65ae2338ae2c6d72e8671aaedf2228bd57319915
Mouiad Ali <mouiad.alilo@gmail.com>
2022-11-18 20:03:07 +0200

    feat: add styles

--- home/models.py
+++ home/models.py
@@ -1,7 +1,17 @@
 from django.db import models
-
+from wagtail.admin.panels import FieldPanel
+from django.utils.translation import gettext_lazy as _
 from wagtail.models import Page
 
 
 class HomePage(Page):
-    pass
+    templates = "home/home_page.html"
+    max_count = 1
+    banner_title = models.CharField(_("banner title "), max_length=300, default="")
+
+    content_panels = Page.content_panels + [FieldPanel('banner_title'),]
+
+
+    class Meta:
+        verbose_name = "Home Page"
+        verbose_name_plural = "Home Pages"
--- realestate/settings/base.py
+++ realestate/settings/base.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     "home",
     "search",
 
+    "property",
 
     "wagtail.contrib.forms",
     "wagtail.contrib.redirects",
