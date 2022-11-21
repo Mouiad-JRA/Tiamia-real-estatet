ef1a73236fb23a2543387ec9195a34e0975d7cec
Mouiad Ali <mouiad.alilo@gmail.com>
2022-11-21 20:44:28 +0200

    feat: add image

--- flex/models.py
+++ flex/models.py
@@ -5,6 +5,7 @@ from wagtail.admin.panels import FieldPanel
 from wagtail.core.models import Page
 from wagtail.fields import RichTextField
 from django.utils.translation import gettext_lazy as _
+from wagtail.images.edit_handlers import ImageChooserPanel
 
 
 class FlexPage(Page):
@@ -14,9 +15,19 @@ class FlexPage(Page):
 
     templates = "home/flex_page.html"
 
-    sub_title = RichTextField(_(" Sub title"))
+    sub_title_one = RichTextField(_(" Sub title one"))
+    sub_title_two = RichTextField(_(" Sub title two"))
+    banner_image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name="+"
+    )
     content_panels = Page.content_panels + [
-        FieldPanel('sub_title'),
+        FieldPanel('sub_title_one'),
+        FieldPanel('sub_title_two'),
+        ImageChooserPanel('banner_image'),
 
     ]
 
