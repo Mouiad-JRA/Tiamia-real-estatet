f3c6ca3754568359a65a1018d1f1a830ad751a14
Mouiad Ali <mouiad.alilo@gmail.com>
2022-11-21 20:02:03 +0200

    feat: add html

--- home/models.py
+++ home/models.py
@@ -1,16 +1,42 @@
 from django.db import models
-from wagtail.admin.panels import FieldPanel
+from wagtail.admin.panels import FieldPanel, PageChooserPanel
 from django.utils.translation import gettext_lazy as _
+from wagtail.fields import RichTextField
+from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.models import Page
 
 
 class HomePage(Page):
     templates = "home/home_page.html"
     max_count = 1
-    banner_title = models.CharField(_("banner title "), max_length=300, default="")
 
-    content_panels = Page.content_panels + [FieldPanel('banner_title'),]
+    banner_title = models.CharField(
+        _("banner subtitle "), max_length=300, default="")
+    search_placeholder = models.CharField(
+        _("search placeholder "), max_length=300, default="")
+    banner_image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name="+"
+    )
 
+    banner_cta = models.ForeignKey(
+        "wagtailcore.Page",
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name="+"
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel('banner_title'),
+        FieldPanel('search_placeholder'),
+        ImageChooserPanel('banner_image'),
+        PageChooserPanel('banner_cta'),
+
+    ]
 
     class Meta:
         verbose_name = "Home Page"
